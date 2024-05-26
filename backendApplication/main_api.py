412f15c8dee5adc1aa84253d0dfd2e3a9c58d99d
Dorian Boucher <dorian.bch44@gmail.com>
2024-05-26 00:59:33 +0000

    adding yml config file

--- backendApplication/main_api.py
+++ backendApplication/main_api.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import langchain_core
 import json
+from retrieval import retrieval
+import yaml
 
 # Importer votre fonction de traitement du texte
 from generatesummaryfromdocuments import generate_summary_from_document
 
 app = FastAPI()
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+api_key = config["api_keys"]["mistral_api_key"]
+llm_model = config["models"]["llm_model_name"]
+embedding_model = config["models"]["embedding_model_name"]
 
 class ChapterSummaryRequest(BaseModel):
     chapter_selected: str
@@ -18,7 +27,10 @@ class ChapterSummaryResponse(BaseModel):
 
 @app.post("/generate-summary", response_model=ChapterSummaryResponse)
 def process_text_endpoint(request: ChapterSummaryRequest):
-    output_text = generate_summary_from_document(request.chapter_selected, request.difficulty_selected)
+
+    retrieve_cunks_str = retrieval("hackaton-mistral-studai/data/chromadb", api_key, request.chapter_selected)
+
+    output_text = generate_summary_from_document(retrieve_cunks_str, request.difficulty_selected)
 
     return {"chapter_summary":output_text}
 
